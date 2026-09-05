e1cb416049346775275d77fc684ac3a57fac4b64
carty <148784495+moononuu@users.noreply.github.com>
2026-09-05 16:31:28 +0100

    Patrolling the Perimeter

--- Assets/State.cs
+++ Assets/State.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.AI;
 using UnityEngine.Jobs;
+using UnityEngine.Rendering;
 
 public class State : MonoBehaviour
 {
@@ -55,3 +56,66 @@ public class State : MonoBehaviour
     }
 
 }
+
+public class Idle : State
+{
+    public Idle(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player) : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.IDLE;
+    }
+    public override void Enter()
+    {
+        anim.SetTrigger("isIdle");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+        {
+            nextState = new Patrol(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
+        base.Update();
+    }
+
+    public override void Exit()
+    {
+        anim.ResetTrigger("isIdle");
+        base.Exit();
+    }
+
+    public class Patrol : State
+    {
+
+        int currentIndex = -1;
+        public Patrol(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player) : base(_npc, _agent, _anim, _player)
+        {
+            name = STATE.PATROL;
+            agent.speed = 2;
+            agent.isStopped = false;
+        }
+        public override void Enter()
+        {
+            currentIndex = 0;
+            anim.SetTrigger("isWalking");
+            base.Enter();
+        }
+        public override void Update()
+        {
+            if (currentIndex >= GameEnvironment.Singleton.Checkpoints.Count)
+                currentIndex = 0;
+            else
+                currentIndex++;
+
+            agent.SetDestination(GameEnvironment.Singleton.Checkpoints[currentIndex].transform.position);
+            base.Update();
+        }
+        public override void Exit()
+        {
+            anim.ResetTrigger("isWalking");
+            base.Exit();
+        }
+
+    } }
+
